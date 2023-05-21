fa3b66c4246a9d3919f439104a856fe3da8acde1
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2023-05-21 13:50:40 -0300

    Project Created

---
+++ src/test/java/br/com/pauloricardodev/dslist/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pauloricardodev.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
